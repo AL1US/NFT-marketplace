5565fe0d263c6974068ed09d031fa0642d5ca0c9
AL1US <Matvey.Ale.Devs@gmail.com>
2025-11-28 02:46:18 -0800

    Коллекции в магазин + фикс некоторых ошибок

--- NFT/main.sol
+++ NFT/main.sol
@@ -12,19 +12,33 @@ contract Main is Xcoin, XcoinNFT, ERC1155Holder {
 
     // Нужен для того, чтобы если несколько юзеров захотели добавить nft с одинаковым id в магазин,
     // то данные в мапинге не перезаписались, а добавились новые
-    uint256 indexNFTInStore;
+    uint256 public indexNFTInStore;
+    uint256 public unicueCollectionNFTInStore;
 
-    // Для того чтобы не засорять магазин ненужными данными, можно создать структуру только с теми данными
-    // которые будет нам нужны
+    /* Струкутры
+    * Для того чтобы не засорять магазин ненужными данными, можно создать структуру только с теми данными
+    * которые будет нам нужны
+    */ 
+
     struct structNFTsInStore {
         uint256 id;
         address owner;
         uint256 amount;
         uint256 price;
     }
-    // индекс в магазине => айди => наш продукт в магазине
+
+    struct structCollectionInStore {
+        uint256 id;
+        address owner;
+        uint256 price;
+    }
+
+    /*
+    * Мапинги
+    */ 
+
     mapping(uint256 => structNFTsInStore) public storeNFT; // index => struct
-    mapping(uint256 => structNFTsInStore) public storeCollectionNFT;
+    mapping(uint256 => structCollectionInStore) public storeCollectionNFT;
 
     /*
     * Get функции с nft и коллекциями в магазине
@@ -43,6 +57,8 @@ contract Main is Xcoin, XcoinNFT, ERC1155Holder {
     function setNFTInStore(uint256 _id, uint256 _amount, uint256 _price) public {
         require(NFT[msg.sender][_id].amount >= _amount, "You don't have this NFT");
         require(_amount > 0, "Amount must be > 0");
+        require( isApprovedForAll(msg.sender, address(this)), "Please approve the marketplace");
+
         
         bytes memory data = "";
 
@@ -110,7 +126,39 @@ contract Main is Xcoin, XcoinNFT, ERC1155Holder {
             delete storeNFT[_index];
         }
 
+    }
 
+    function setCollectionInStore(uint256 _id, uint256 _price) public {
+        require(collectionNFTs[msg.sender][_id].existence, "You don't have this collection");
+        require(!collectionNFTs[msg.sender][_id].state, "Collection already in store");
+        require( isApprovedForAll(msg.sender, address(this)), "Please approve the marketplace");
+
+        structNFTsInCollection[] storage col = collectionNFTs[msg.sender][_id].NFTInCollection;
+        require(col.length > 0, "Collection is empty");
+
+        // Собираем массивы для batch transfer
+        uint256[] memory ids = new uint256[](col.length);
+        uint256[] memory amounts = new uint256[](col.length);
+
+        for (uint256 i = 0; i < col.length; i++) {
+            ids[i] = col[i].id;
+            amounts[i] = col[i].amount;
+        }
+
+        // Передаём всю коллекцию контракту
+        safeBatchTransferFrom(msg.sender, address(this), ids, amounts, "");
+
+        // Сохраняем коллекцию в магазин
+        storeCollectionNFT[unicueCollectionNFTInStore] = structCollectionInStore(
+            _id,
+            msg.sender,
+            _price
+        );
+
+        // Отмечаем что она в магазине
+        collectionNFTs[msg.sender][_id].state = true;
+
+        unicueCollectionNFTInStore++;
     }
 
     // Эта штука как то решает проблему с тем, что этот контракт не может принимать nft
