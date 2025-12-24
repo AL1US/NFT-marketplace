967e898f2ca4f368476ed15b5be88b5601166550
AL1US <193784287+AL1US@users.noreply.github.com>
2025-12-24 14:20:45 -0800

    add good header

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, request, redirect, session, jsonify, url_for
 
 from src.blockchain.client import contract_client
-
 from src.user.account import user_app
 from src.user.getFunctions import profile_app
 from src.nft.setFunctions import nft_app
-from src.errors.error_handlers import error_app
-
 
 app = Flask(__name__)
 app.secret_key = 'secret_key' 
@@ -14,12 +11,11 @@ app.secret_key = 'secret_key'
 app.register_blueprint(user_app)
 app.register_blueprint(profile_app)
 app.register_blueprint(nft_app)
-app.register_blueprint(error_app)
 
-
+# 404 обработчик
 @app.errorhandler(404)
-def pageNotFount(error):
-  return render_template('page404.html')
+def page_not_found(error):
+    return render_template('page404.html'), 404
 
 @app.route("/")
 def index():
--- app/src/errors/error_handlers.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, redirect, Blueprint
-
-error_app = Blueprint("error", __name__)
-
-
-@error_app.errorhandler(404)
-def pageNotFount(error):
-  return render_template('page404.html')
